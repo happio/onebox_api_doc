3439348718eb1da5ece62b5c116f01be9080c4e5
Carryall <carryall369@gmail.com>
2015-05-28 17:20:17 +0700

    add method to unload doc file

--- lib/onebox_api_doc/base.rb
+++ lib/onebox_api_doc/base.rb
@@ -18,11 +18,23 @@ module OneboxApiDoc
       Dir.glob(Rails.root.join(*OneboxApiDoc::Engine.api_docs_matcher.split("/")))
     end
 
+    def unload_documentation
+      api_docs_paths.each do |f|
+        unload_api_doc_from_file f
+      end
+    end
+
     private
 
     def load_api_doc_from_file(api_doc_file)
       require api_doc_file
       api_doc_class_name = api_doc_file.gsub(/\A.*\/api_doc\//,"").gsub(/\.\w*\Z/,"").camelize
+    end
+
+    def unload_api_doc_from_file(api_doc_file)
+      api_doc_class_name = api_doc_file.gsub(/\A.*\/api_doc\//,"").gsub(/\.\w*\Z/,"").camelize
+      klass = Object.const_get(api_doc_class_name) rescue nil
+      Object.send(:remove_const, api_doc_class_name) if klass.is_a?(Class)
     end
 
     # Since Rails 3.2, the classes are reloaded only on file change.
@@ -32,7 +44,7 @@ module OneboxApiDoc
     # when showing the documentation.
     #
     # If cache_classes is set to false, it does nothing,
-    # as this would break loading of the controllers.
+    # as this would break loading of the api doc.
     def rails_mark_classes_for_reload
       unless Rails.application.config.cache_classes
         ActionDispatch::Reloader.cleanup!
