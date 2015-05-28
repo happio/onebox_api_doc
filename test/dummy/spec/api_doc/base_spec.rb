dd398d01d6cefcd6171419d4ce2784e867c8e968
Carryall <carryall369@gmail.com>
2015-05-28 17:17:40 +0700

    update test cases of base class

--- test/dummy/spec/api_doc/base_spec.rb
+++ test/dummy/spec/api_doc/base_spec.rb
@@ -4,15 +4,32 @@ module OneboxApiDoc
   describe Base do
 
     describe "reload_documentation" do
-      it "error when call document class name if not load before" do
-        expect{ UsersApiDoc }.to raise_error NameError
+      before :each do
+        OneboxApiDoc::Base.new.unload_documentation
       end
 
-      it "not error when call document class name after load" do
+      it "load document class" do
         OneboxApiDoc::Base.new.reload_documentation
         expect{ UsersApiDoc }.not_to raise_error
       end
-      
+    end
+
+    describe "unload_documentation" do
+      it "does not load document class if the class wasn't load yet" do
+        OneboxApiDoc::Base.new.unload_documentation
+        expect{ UsersApiDoc }.to raise_error
+      end
+      it "unload document class if the class was loaded" do
+        OneboxApiDoc::Base.new.reload_documentation
+        OneboxApiDoc::Base.new.unload_documentation
+        expect{ UsersApiDoc }.to raise_error
+      end
+    end
+
+    describe "api_docs_paths" do
+      it "return correct array of document paths" do
+        expect(OneboxApiDoc::Base.new.api_docs_paths).to eq Dir.glob(Rails.root.join(*OneboxApiDoc::Engine.api_docs_matcher.split("/")))
+      end
     end
   end
 end
