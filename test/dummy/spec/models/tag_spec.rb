eda5515b3384c854682771b75fcbb13a9b4d9d3c
Carryall <carryall369@gmail.com>
2015-06-04 16:18:12 +0700

    remove find or init method

--- test/dummy/spec/models/tag_spec.rb
+++ test/dummy/spec/models/tag_spec.rb
@@ -2,19 +2,6 @@ require "rails_helper"
 
 module OneboxApiDoc
   describe Tag do
-    
-    describe "find_or_initialize" do
-      it "create a new tag if tag with given name does not exist" do
-        expect(OneboxApiDoc::Tag).to receive(:new)
-        new_tag = OneboxApiDoc::Tag.find_or_initialize :new_tag
-      end
-      it "do not create a new tag if tag with given name already not exist" do
-        tag = OneboxApiDoc::Tag.new :new_tag
-        expect(OneboxApiDoc::Tag).not_to receive(:new)
-        OneboxApiDoc::Tag.find_or_initialize :new_tag
-      end
-    end
-
     describe "initialize" do
       it "set correct name and init blank apis" do
         tag = OneboxApiDoc::Tag.new :new_tag
