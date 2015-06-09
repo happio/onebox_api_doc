5217c06f02d6bfc9f03ef84054090afcf6a954da
Carryall <carryall369@gmail.com>
2015-06-09 17:50:59 +0700

    reuse base object

--- test/dummy/spec/api_doc/base_spec.rb
+++ test/dummy/spec/api_doc/base_spec.rb
@@ -3,40 +3,34 @@ require "rails_helper"
 module OneboxApiDoc
   describe Base do
 
+    let(:base) { OneboxApiDoc::Base.new }
+
     describe "reload_documentation" do
-      before do
-        @base = OneboxApiDoc::Base.new
-      end
       it "unload document and load it again" do
-        expect(@base).to receive :load_documentation
-        expect(@base).to receive :unload_documentation
-        @base.reload_documentation
+        expect(base).to receive :load_documentation
+        expect(base).to receive :unload_documentation
+        base.reload_documentation
       end
     end
 
     describe "load_documentation" do
-      before do
-        @base = OneboxApiDoc::Base.new
-      end
       it "load document class" do
-        @base.load_documentation
+        base.load_documentation
         expect{ UsersApiDoc }.not_to raise_error
       end
     end
 
     describe "unload_documentation" do
       it "does not load document class if the class wasn't load yet" do
-        OneboxApiDoc::Base.new.unload_documentation
+        base.unload_documentation
         expect{ UsersApiDoc }.to raise_error
       end
       it "unload document class if the class was loaded" do
-        base = OneboxApiDoc::Base.new
         base.load_documentation
         base.unload_documentation
         expect{ UsersApiDoc }.to raise_error
       end
       it "reset attributes to default value" do
-        base = OneboxApiDoc::Base.new
         expect(base).to receive :set_default_value
         base.unload_documentation
       end
@@ -44,19 +38,19 @@ module OneboxApiDoc
 
     describe "api_docs_paths" do
       it "return correct array of document paths" do
-        expect(OneboxApiDoc::Base.new.api_docs_paths).to eq Dir.glob(Rails.root.join(*OneboxApiDoc::Engine.api_docs_matcher.split("/")))
+        expect(base.api_docs_paths).to eq Dir.glob(Rails.root.join(*OneboxApiDoc::Engine.api_docs_matcher.split("/")))
       end
     end
 
     describe "api_docs" do
       it "return all api doc classes" do
         expected_api_docs = OneboxApiDoc::ApiDoc.subclasses
-        expect(OneboxApiDoc::Base.new.api_docs).to eq expected_api_docs
+        expect(base.api_docs).to eq expected_api_docs
       end
     end
 
     describe "get_api" do
-      before :each do
+      before do
         @base = OneboxApiDoc.base
         @base.load_documentation
       end
@@ -82,13 +76,11 @@ module OneboxApiDoc
 
     describe "add_tag" do
       it "add tag to @all_tags" do
-        base = OneboxApiDoc::Base.new
         base.add_tag :new_tag
         expect(base.all_tags.size).to eq 1
         expect(base.all_tags.map(&:name)).to include "new_tag"
       end
       it "does not add tag with duplicate name" do
-        base = OneboxApiDoc::Base.new
         base.add_tag :new_tag
         base.add_tag :new_tag
         expect(base.all_tags.size).to eq 1
@@ -98,14 +90,12 @@ module OneboxApiDoc
 
     describe "add_api" do
       it "add api to @all_apis" do
-        base = OneboxApiDoc::Base.new
         api = OneboxApiDoc::Api.new(:products, :index)
         base.add_api api
         expect(base.all_apis.size).to eq 1
         expect(base.all_apis).to include api
       end
       it "does not add duplicate api to @all_apis" do
-        base = OneboxApiDoc::Base.new
         api = OneboxApiDoc::Api.new(:products, :index)
         base.add_api api
         base.add_api api
@@ -116,13 +106,11 @@ module OneboxApiDoc
 
     describe "add_version" do
       it "add version to @core_versions" do
-        base = OneboxApiDoc::Base.new
         base.add_version "1.2"
         expect(base.core_versions.size).to eq 1
         expect(base.core_versions.map(&:version)).to include "1.2"
       end
       it "does not add version with duplicate name" do
-        base = OneboxApiDoc::Base.new
         base.add_version "1.2"
         base.add_version "1.2"
         expect(base.core_versions.size).to eq 1
@@ -132,13 +120,11 @@ module OneboxApiDoc
 
     describe "add_extension_version" do
       it "add extension version to @extension_versions" do
-        base = OneboxApiDoc::Base.new
         base.add_extension_version :extension_name, "1.2"
         expect(base.extension_versions["extension_name"].size).to eq 1
         expect(base.extension_versions["extension_name"].map(&:version)).to include "1.2"
       end
       it "does not add extension version with duplicate name" do
-        base = OneboxApiDoc::Base.new
         base.add_extension_version :extension_name, "1.2"
         base.add_extension_version :extension_name, "1.2"
         expect(base.extension_versions["extension_name"].size).to eq 1
@@ -148,13 +134,11 @@ module OneboxApiDoc
 
     describe "add_permission" do
       it "add permission to @all_permissions" do
-        base = OneboxApiDoc::Base.new
         base.add_permission :admin
         expect(base.all_permissions.size).to eq 1
         expect(base.all_permissions).to include "admin"
       end
       it "do not add duplicate permission to @all_permissions" do
-        base = OneboxApiDoc::Base.new
         base.add_permission "admin"
         base.add_permission :admin
         expect(base.all_permissions.size).to eq 1
@@ -164,7 +148,6 @@ module OneboxApiDoc
 
     describe "add_param_group" do
       it "add param group" do
-        base = OneboxApiDoc::Base.new
         base.add_param_group :sample do
           puts "do something"
         end
@@ -175,12 +158,11 @@ module OneboxApiDoc
 
     describe "get_param_group" do
       before do
-        @base = OneboxApiDoc::Base.new
         @block = Proc.new { puts "do something" }
-        @base.param_groups["sample"] = @block
+        base.param_groups["sample"] = @block
       end
       it "return correct proc" do
-        param_group = @base.get_param_group :sample
+        param_group = base.get_param_group :sample
         expect(param_group).to be_a Proc
         expect(param_group).to eq @block
       end
