b8f97476241592e7dbf729911b1b54d8518cd58b
Carryall <carryall369@gmail.com>
2015-06-11 11:16:31 +0700

    add route for main controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 OneboxApiDoc::Engine.routes.draw do
+
+  root 'application#index'
+
+  get '/:version/:resource_name/:action_name', to: 'application#index', :constraints => { version: /[^\/]+/ }
+  get '/:version/:resource_name', to: 'application#index', :constraints => { version: /[^\/]+/ }
+
 end
