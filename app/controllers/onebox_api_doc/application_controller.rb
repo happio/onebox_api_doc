721358c4eeae7c2b6d406495d5482dd934248bab
Carryall <carryall369@gmail.com>
2015-06-11 11:17:49 +0700

    define base, load doc and find api or apis

--- app/controllers/onebox_api_doc/application_controller.rb
+++ app/controllers/onebox_api_doc/application_controller.rb
@@ -2,10 +2,23 @@ module OneboxApiDoc
   class ApplicationController < ActionController::Base
 
     def index
-
+      base = OneboxApiDoc.base
+      base.reload_documentation
+      if api_params[:version].present?
+        if api_params[:resource_name].present? and api_params[:action_name].present?
+          @api = base.get_api(api_params[:version], api_params[:resource_name], api_params[:action_name])
+        elsif api_params[:resource_name].present?
+          @apis = base.get_api(api_params[:version], api_params[:resource_name])
+        end
+      end
+      render nothing: true
     end
 
-    
+    private
+
+    def api_params
+      params.permit(:version, :resource_name, :action_name)
+    end
 
   end
 end
