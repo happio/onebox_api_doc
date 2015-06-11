511fb8b83b80b7f9e14b9cf01f1b0813312e3b53
Carryall <carryall369@gmail.com>
2015-06-11 11:16:10 +0700

    change engine mounted path

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  mount OneboxApiDoc::Engine => "/onebox_api_doc"
+  mount OneboxApiDoc::Engine => "/api_doc"
 
   resources :products, only: [:index, :create, :show, :update, :destroy]
   resources :users, only: [:show, :update]
