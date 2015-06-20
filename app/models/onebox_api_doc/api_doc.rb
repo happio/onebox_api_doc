ef0a8ecf09d1cdf26fef52bec6708c0de754c7dc
Carryall <carryall369@gmail.com>
2015-06-20 15:41:29 +0700

    rewrite apidoc model

--- app/models/onebox_api_doc/api_doc.rb
+++ app/models/onebox_api_doc/api_doc.rb
@@ -1,59 +1,150 @@
 module OneboxApiDoc
   class ApiDoc
 
+    attr_accessor :tags, :permissions, :apis, :params, :errors
+    attr_accessor :version_id, :resource_id
+
+    def initialize version_id, resource_id
+      self.version_id = version_id
+      self.resource_id = resource_id
+      self.tags = []
+      self.permissions = []
+      self.apis = []
+      self.params = []
+      self.errors = []
+    end
+
+    def resource
+      OneboxApiDoc.base.resources.select { |resource| resource.object_id == self.resource_id }.first
+    end
+
+    def add_api action, short_desc, &block
+      resource_name = self.resource.name
+      route = Route.route_for(resource_name, action)
+      return false unless route.present?
+      url = route[:path]
+      method = route[:method]
+      unless self.apis.map(&:action).include? action
+        api = OneboxApiDoc::Api.new(self.object_id, self.resource_id, action, method, url, short_desc)
+        self.apis << api
+      else
+        api = self.apis.select { |api| api.action == action }.first
+      end
+      OneboxApiDoc::ApiDefinition.new(api, &block) if block_given?
+      api
+    end
+
+    # def add_param name, type, options={}, &block
+    #   param = OneboxApiDoc::Param.new(name, type, options)
+    #   ParamContainerDefinition.new(param, &block) if block_given?
+    #   self.params << param
+    # end
+
+    def add_tag tag_name
+      unless self.tags.map(&:name).include? tag_name.to_s
+        tag = OneboxApiDoc::Tag.new(tag_name)
+        tags << tag
+        tag
+      else
+        tags.select { |tag| tag.name == tag_name }.first
+      end
+    end
+
+    def add_permission permission_name
+      unless self.permissions.map(&:name).include? permission_name.to_s
+        permission = OneboxApiDoc::Permission.new(permission_name)
+        permissions << permission
+        permission
+      else
+        permissions.select { |permission| permission.name == permission_name }.first
+      end
+    end
+
     class << self
+
+      cattr_accessor :api_doc
 
       attr_accessor :_controller_name, :_version, :_default_version, :_apis
       # for extension
       attr_accessor :_extension_name, :_core_versions
       
       def inherited(subclass)
-        subclass._controller_name = subclass.name.demodulize.gsub(/ApiDoc/,"").pluralize.underscore
-        subclass._version = OneboxApiDoc.base.default_version
-        subclass._core_versions = []
+        # subclass._controller_name = subclass.name.demodulize.gsub(/ApiDoc/,"").pluralize.underscore
+        # subclass._version = OneboxApiDoc.base.default_version
+        # subclass._core_versions = []
+
+        resource_name = subclass.name.demodulize.gsub(/ApiDoc/,"").pluralize.underscore
+        resource = OneboxApiDoc.base.add_resource(resource_name)
+        version = OneboxApiDoc.base.default_version
+        self.api_doc = OneboxApiDoc.base.add_doc(subclass, version.object_id, resource.object_id)
       end
 
       ##############################
       ####### Setter Methods #######
       ##############################
 
+      # def controller_name name
+      #   @_controller_name = name.to_s
+      # end
       def controller_name name
-        @_controller_name = name.to_s
+        self.api_doc.resource_id = OneboxApiDoc.base.add_resource(name).object_id
       end
 
       # for extension
-      def extension_name name
-        @_extension_name = name.to_s
-      end
+      # def extension_name name
+      #   @_extension_name = name.to_s
+      # end
 
       # core_version is for extension
-      def version version, core_version: {}
+      # def version version, core_version: {}
+      #   if core_version.blank?
+      #     @_version = OneboxApiDoc.base.add_version version.to_s
+      #   elsif @_extension_name.present?
+      #     @_version = OneboxApiDoc.base.add_extension_version @_extension_name, version.to_s
+      #     set_core_versions core_version
+      #   end
+      # end
+
+      # core_version is for extension
+      def version name, core_version: {}
         if core_version.blank?
-          @_version = OneboxApiDoc.base.add_version version.to_s
-        elsif @_extension_name.present?
-          @_version = OneboxApiDoc.base.add_extension_version @_extension_name, version.to_s
-          set_core_versions core_version
+          self.api_doc.version_id = OneboxApiDoc.base.add_version(name).object_id
+        else
+          self.api_doc.version_id = OneboxApiDoc.base.add_extension_version(name).object_id
         end
       end
 
+      # def api action, short_desc="", &block
+      #   @_apis ||= []
+      #   api = OneboxApiDoc::Api.new(_controller_name, action, short_desc, &block)
+      #   @_apis << api
+      #   @_version.apis << api
+      #   OneboxApiDoc.base.add_api api
+      # end
       def api action, short_desc="", &block
-        @_apis ||= []
-        api = OneboxApiDoc::Api.new(_controller_name, action, short_desc, &block)
-        @_apis << api
-        @_version.apis << api
-        OneboxApiDoc.base.add_api api
+        self.api_doc.add_api action, short_desc, &block
       end
 
-      def def_param_group name, &block
-        OneboxApiDoc.base.add_param_group name, &block
-      end
+      # def def_param_group name, &block
+      #   OneboxApiDoc.base.add_param_group name, &block
+      # end
 
-      def set_core_versions core_version
-        # all_core_versions = OneboxApiDoc.base.core_versions
-        # ...
-        # TODO: set core version incase of this is extension api doc
-      end
+      # def set_core_versions core_version
+      #   # all_core_versions = OneboxApiDoc.base.core_versions
+      #   # ...
+      #   # TODO: set core version incase of this is extension api doc
+      # end
 
+    end
+
+    private
+
+    def set_default_value
+      self.tags = []
+      self.permissions = []
+      self.apis = []
+      self.params = []
+      self.errors = []
     end
 
   end
