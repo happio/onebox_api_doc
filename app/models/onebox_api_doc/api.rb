a1e06e80510485dda8739207b1bf2161e51d4859
Carryall <carryall369@gmail.com>
2015-06-20 15:41:02 +0700

    rewrite api model

--- app/models/onebox_api_doc/api.rb
+++ app/models/onebox_api_doc/api.rb
@@ -1,106 +1,129 @@
 module OneboxApiDoc
   class Api
 
-    attr_reader :_controller_name, :_action, :_method, :_url, :_permissions, :_short_desc,
-      :_desc, :_tags, :_header, :_body, :_response, :_error, :_api_doc
+    # attr_reader :_controller_name, :_action, :_method, :_url, :_permissions, :_short_desc,
+    #   :_desc, :_tags, :_header, :_body, :_response, :_error, :_api_doc
 
-    def initialize controller_name, action, short_desc="", &block
-      route = Route.route_for(controller_name, action)
-      return nil unless route.present?
-      @_controller_name = controller_name.to_s
-      @_url = route[:path]
-      @_method = route[:method]
-      @_action = action.to_s
-      @_short_desc = short_desc
-      @_permissions = []
-      @_tags = []
-      self.instance_eval(&block) if block_given?
+    attr_accessor :resource_id, :version_id, :doc_id
+    attr_accessor :action, :method, :url, :permission_ids, :desc, :short_desc, :tag_ids, :error_ids
+    attr_accessor :request
+    attr_accessor :response
+
+    def initialize doc_id, resource_id, action, method, url, short_desc="", options={}
+      self.doc_id = doc_id
+      self.resource_id = resource_id
+      self.action = action.to_sym
+      self.method = method.to_s.upcase.to_sym
+      self.url = url
+      self.short_desc = short_desc
+      self.desc = options[:desc]
+      self.tag_ids = options[:tag_ids]
+      self.error_ids = options[:error_ids]
+      self.request = { header: [], body: [] }
+      self.response = { header: [], body: [] }
     end
 
-    ##############################
-    ####### Setter Methods #######
-    ##############################
+    # def initialize controller_name, action, short_desc="", &block
+    #   route = Route.route_for(controller_name, action)
+    #   return nil unless route.present?
+    #   @_controller_name = controller_name.to_s
+    #   @_url = route[:path]
+    #   @_method = route[:method]
+    #   @_action = action.to_s
+    #   @_short_desc = short_desc
+    #   @_permissions = []
+    #   @_tags = []
+    #   self.instance_eval(&block) if block_given?
+    # end
 
-    def desc desc
-      @_desc = desc
+    def doc
+      OneboxApiDoc.base.docs.select { |doc| doc.object_id == self.doc_id }.first
     end
 
-    def tags *tags
-      @_tags = tags.map do |tag|
-        t = OneboxApiDoc.base.add_tag(tag.to_s)
-        t.apis << self
-        t
-      end
-    end
+    # ##############################
+    # ####### Setter Methods #######
+    # ##############################
 
-    def permissions *permissions
-      @_permissions = permissions.map{ |permission| OneboxApiDoc.base.add_permission permission }
-    end
+    # def desc desc
+    #   @_desc = desc
+    # end
 
-    def header &block
-      @_header = Header.new(&block) if block_given?
-    end
+    # def tags *tags
+    #   @_tags = tags.map do |tag|
+    #     t = OneboxApiDoc.base.add_tag(tag.to_s)
+    #     t.apis << self
+    #     t
+    #   end
+    # end
 
-    def body &block
-      @_body = Body.new(&block) if block_given?
-    end
+    # def permissions *permissions
+    #   @_permissions = permissions.map{ |permission| OneboxApiDoc.base.add_permission permission }
+    # end
 
-    def response &block
-      @_response = Response.new(&block) if block_given?
-    end
+    # def header &block
+    #   @_header = Header.new(&block) if block_given?
+    # end
 
-    def error &block
-      @_error = Error.new(&block) if block_given?
-    end
+    # def body &block
+    #   @_body = Body.new(&block) if block_given?
+    # end
 
-    ##############################
-    ####### Nested Classes #######
-    ##############################
+    # def response &block
+    #   @_response = Response.new(&block) if block_given?
+    # end
 
-    class Header < ParamContainer
-    end
+    # def error &block
+    #   @_error = Error.new(&block) if block_given?
+    # end
 
-    class Body < ParamContainer
-    end
+    # ##############################
+    # ####### Nested Classes #######
+    # ##############################
 
-    class Response < ParamContainer
-    end
+    # class Header < ParamContainer
+    # end
 
-    class Error
-      attr_reader :_codes
+    # class Body < ParamContainer
+    # end
 
-      def initialize &block
-        @_codes ||= []
-        self.instance_eval(&block) if block_given?
-      end
+    # class Response < ParamContainer
+    # end
 
-      def code error_code, message="", &block
-        @_codes << Code.new(error_code, message, &block)
-      end
+    # class Error
+    #   attr_reader :_codes
 
-      class Code < ParamContainer
-        attr_reader :_code, :_message, :_permissions
+    #   def initialize &block
+    #     @_codes ||= []
+    #     self.instance_eval(&block) if block_given?
+    #   end
 
-        def initialize code, message, &block
-          @_code = code
-          @_message = message
-          @_params = []
-          @_permissions = []
-          self.instance_eval(&block) if block_given?
-        end
+    #   def code error_code, message="", &block
+    #     @_codes << Code.new(error_code, message, &block)
+    #   end
 
-        ##############################
-        ####### Setter Methods #######
-        ##############################
+    #   class Code < ParamContainer
+    #     attr_reader :_code, :_message, :_permissions
 
-        def permissions *permissions
-          @_permissions = permissions.map{ |permission| OneboxApiDoc.base.add_permission permission }
-        end
+    #     def initialize code, message, &block
+    #       @_code = code
+    #       @_message = message
+    #       @_params = []
+    #       @_permissions = []
+    #       self.instance_eval(&block) if block_given?
+    #     end
 
-        def param name="", type, desc: "", permissions: [], &block
-          @_params << OneboxApiDoc::Param.new(name, type, desc: desc, permissions: permissions, &block)
-        end
-      end
-    end
+    #     ##############################
+    #     ####### Setter Methods #######
+    #     ##############################
+
+    #     def permissions *permissions
+    #       @_permissions = permissions.map{ |permission| OneboxApiDoc.base.add_permission permission }
+    #     end
+
+    #     def param name="", type, desc: "", permissions: [], &block
+    #       @_params << OneboxApiDoc::Param.new(name, type, desc: desc, permissions: permissions, &block)
+    #     end
+    #   end
+    # end
   end
 end
