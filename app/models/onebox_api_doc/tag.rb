438dc5ae9dc9ae51840aa0a419c261bf0b2aee37
Carryall <carryall369@gmail.com>
2015-06-20 15:40:43 +0700

    rewrite tag model

--- app/models/onebox_api_doc/tag.rb
+++ app/models/onebox_api_doc/tag.rb
@@ -1,21 +1,27 @@
 module OneboxApiDoc
   class Tag
     
-    attr_accessor :name, :apis
+    # attr_accessor :name, :apis
+
+    # def initialize name
+    #   self.name = name.to_s
+    #   self.apis = []
+    #   self
+    # end
+
+    # def apis_by_resources
+    #   result = {}
+    #   apis.each do |api|
+    #     result[api._controller_name] ||= []
+    #     result[api._controller_name] << api
+    #   end
+    #   result
+    # end
+
+    attr_accessor :name
 
     def initialize name
       self.name = name.to_s
-      self.apis = []
-      self
-    end
-
-    def apis_by_resources
-      result = {}
-      apis.each do |api|
-        result[api._controller_name] ||= []
-        result[api._controller_name] << api
-      end
-      result
     end
   end
 end
