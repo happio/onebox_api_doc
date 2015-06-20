5478df8e37d0267c6140957d2a03d8db28fb5723
Carryall <carryall369@gmail.com>
2015-06-20 15:40:27 +0700

    rewrite version model

--- app/models/onebox_api_doc/version.rb
+++ app/models/onebox_api_doc/version.rb
@@ -1,35 +1,42 @@
 module OneboxApiDoc
   class Version
     
-    attr_accessor :extension_name, :version, :apis
+    # attr_accessor :extension_name, :version, :apis
 
-    def initialize version, extension_name = nil
-      self.extension_name = extension_name.to_s if extension_name.present?
-      self.version = version.to_s
-      self.apis = []
-      self
+    # def initialize version, extension_name = nil
+    #   self.extension_name = extension_name.to_s if extension_name.present?
+    #   self.version = version.to_s
+    #   self.apis = []
+    #   self
+    # end
+
+    attr_accessor :app_id, :name
+
+    def initialize name, app_id
+      self.name = name.to_s
+      self.app_id = app_id
     end
 
     def is_extension?
-      extension_name.present?
+      OneboxApiDoc.base.apps.select { |app| app.object_id == self.app_id }.first.is_extension?
     end
 
-    def get_api(resource_name, action=nil)
-      if action.present?
-        apis.select { |api| api._controller_name == resource_name.to_s and api._action == action.to_s }.first
-      else
-        apis.select { |api| api._controller_name == resource_name.to_s }
-      end
-    end
+    # def get_api(resource_name, action=nil)
+    #   if action.present?
+    #     apis.select { |api| api._controller_name == resource_name.to_s and api._action == action.to_s }.first
+    #   else
+    #     apis.select { |api| api._controller_name == resource_name.to_s }
+    #   end
+    # end
 
-    def apis_group_by_resources
-      result = {}
-      apis.each do |api|
-        result[api._controller_name] ||= []
-        result[api._controller_name] << api
-      end
-      result
-    end
+    # def apis_group_by_resources
+    #   result = {}
+    #   apis.each do |api|
+    #     result[api._controller_name] ||= []
+    #     result[api._controller_name] << api
+    #   end
+    #   result
+    # end
     
   end
 end
