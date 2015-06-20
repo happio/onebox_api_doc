367b9bf8f2a0de1a58d28ac58358d1d23dec3cf4
Carryall <carryall369@gmail.com>
2015-06-20 15:41:59 +0700

    rewrite base unfinished

--- lib/onebox_api_doc/base.rb
+++ lib/onebox_api_doc/base.rb
@@ -1,13 +1,21 @@
 module OneboxApiDoc
   class Base
 
-    attr_reader :all_tags, :all_apis, :all_permissions, :default_version,
+    attr_reader :all_tags, :all_apis, :all_permissions, #:default_version,
       :core_versions, :extension_versions, :param_groups
+
+
+    attr_accessor :apps, :versions, :resources, :docs, :params
+    attr_writer :main_app#, :default_version
+
+    # attributes for indexing
+    attr_reader :index
 
     def initialize
       set_default_value
     end
 
+    # loading documentation
     def reload_documentation
       unload_documentation
       load_documentation
@@ -32,61 +40,129 @@ module OneboxApiDoc
       Dir.glob(Rails.root.join(*OneboxApiDoc::Engine.api_docs_matcher.split("/")))
     end
 
-    def api_docs
-      OneboxApiDoc::ApiDoc.subclasses
+    # main_app
+    def main_app
+      app ||= OneboxApiDoc::App.new("main")
+      self.apps << app unless self.apps.include? app
+      app
     end
 
-    def get_version version_name
-      @core_versions.select { |v| v.version == version_name.to_s }.first
+    # default version
+    def default_version
+      version ||= OneboxApiDoc::Version.new(OneboxApiDoc::Engine.default_version, main_app.object_id)
+      self.versions << version unless self.versions.include? version
+      version
     end
 
-    def get_api version_name, resource_name, action=nil
-      version = get_version(version_name)
-      version.get_api(resource_name, action) if version.present?
-    end
+    # def api_docs
+    #   OneboxApiDoc::ApiDoc.subclasses
+    # end
 
-    def get_tag tag_name
-      @all_tags.select { |tag| tag.name == tag_name.to_s }.first
-    end
+    # def get_version version_name
+    #   @core_versions.select { |v| v.version == version_name.to_s }.first
+    # end
 
-    def add_tag tag_name
-      tag = get_tag tag_name
-      unless tag.present?
-        tag = OneboxApiDoc::Tag.new(tag_name.to_s)
-        @all_tags << tag
+    # def get_api version_name, resource_name, action=nil
+    #   version = get_version(version_name)
+    #   version.get_api(resource_name, action) if version.present?
+    # end
+
+    # def get_tag tag_name
+    #   @all_tags.select { |tag| tag.name == tag_name.to_s }.first
+    # end
+
+    def add_resource resource_name
+      unless self.resources.map(&:name).include? resource_name
+        resource = OneboxApiDoc::Resource.new(resource_name)
+        self.resources << resource
+        resource
+      else
+        self.resources.select { |resource| resource.name == resource_name }.first
       end
-      tag
-    end
-
-    def add_api api
-      @all_apis << api unless @all_apis.include? api
     end
 
     def add_version version_name
-      version = get_version version_name
-      unless version.present?
-        version = OneboxApiDoc::Version.new(version_name.to_s)
-        @core_versions << version
+      unless self.versions.map(&:name).include? version_name
+        version = OneboxApiDoc::Version.new(version_name, main_app.object_id)
+        self.versions << version
+        version
+      else
+        self.versions.select { |version| version.name == version_name }.first
       end
-      version
     end
 
-    def add_extension_version extension_name, version_name
-      all_versions = @extension_versions[extension_name.to_s] ||= []
-      version = all_versions.select { |version| version.version == version_name.to_s  }.first
-      unless version.present?
-        version = OneboxApiDoc::Version.new(version_name.to_s)
-        all_versions << version
+    def add_app app_name
+      unless self.apps.map(&:name).include? app_name
+        app = OneboxApiDoc::App.new(app_name)
+        self.apps << app
+        app
+      else
+        self.apps.select { |app| app.name == app_name }.first
       end
-      version
     end
 
-    def add_permission role
-      role = role.to_s
-      @all_permissions << role unless @all_permissions.include? role
-      role
+    def add_doc klass, version_id, resource_id
+      doc = self.docs.select { |doc| doc.class == klass and doc.version_id == version_id and doc.resource_id == resource_id }.first
+      unless doc.present?
+        doc = klass.new(version_id, resource_id)
+        self.docs << doc
+        doc
+      else
+        doc
+      end
     end
 
+    def add_param name, type, options={}, &block
+      param = OneboxApiDoc::Param.new(name, type, options, &block)
+      self.params << param
+      param
+    end
+
+    # def add_param action, short_desc=""
+    #   OneboxApiDoc::Api.new(action, short_desc, &block)
+    # end
+
+    # def add_tag tag_name
+    #   tag = get_tag tag_name
+    #   unless tag.present?
+    #     tag = OneboxApiDoc::Tag.new(tag_name.to_s)
+    #     @all_tags << tag
+    #   end
+    #   tag
+    # end
+
+    # def add_api api
+    #   @all_apis << api unless @all_apis.include? api
+    # end
+
+    # def add_version version_name
+    #   version = get_version version_name
+    #   unless version.present?
+    #     version = OneboxApiDoc::Version.new(version_name.to_s)
+    #     @core_versions << version
+    #   end
+    #   version
+    # end
+
+    # def add_extension_version extension_name, version_name
+    #   all_versions = @extension_versions[extension_name.to_s] ||= []
+    #   version = all_versions.select { |version| version.version == version_name.to_s  }.first
+    #   unless version.present?
+    #     version = OneboxApiDoc::Version.new(version_name.to_s)
+    #     all_versions << version
+    #   end
+    #   version
+    # end
+
+    # def add_permission role
+    #   role = role.to_s
+    #   @all_permissions << role unless @all_permissions.include? role
+    #   role
+    # end
+
+
+
+    # Params Group
     def add_param_group(name, &block)
       key = name.to_s
       @param_groups[key] = block
@@ -101,16 +177,35 @@ module OneboxApiDoc
       end
     end
 
+    # Model Indexing
+    def next_index klass_name
+      klass_name = klass_name.demodulize.underscore.to_sym
+      @index[klass_name] ||= 0
+      @index[klass_name] += 1
+    end
+
+    # Param Helper
+    def nested_params_of param_id
+      # all_params.select { |param| param.parent_id == param_id }
+    end
+
     private
 
     def set_default_value
-      @all_tags = []
-      @all_permissions = []
-      @all_apis = []
-      @core_versions = []
-      @extension_versions = {}
-      @param_groups = {}
-      @default_version = OneboxApiDoc::Version.new(OneboxApiDoc::Engine.default_version)
+      # @all_tags = []
+      # @all_permissions = []
+      # @all_apis = []
+      # @core_versions = []
+      # @extension_versions = {}
+      # @param_groups = {}
+
+      # @index = {}
+      
+      self.apps = []
+      self.versions = []
+      self.docs = []
+      self.resources = []
+      self.params = []
     end
 
     def load_api_doc_from_file(api_doc_file)
