9b5fae5877483a86c201134c45b374e7bec506ec
Carryall <carryall369@gmail.com>
2015-06-22 17:11:53 +0700

    update api definition

--- lib/onebox_api_doc/api_definition.rb
+++ lib/onebox_api_doc/api_definition.rb
@@ -21,7 +21,7 @@ module OneboxApiDoc
 
     def request &block
       if block_given?
-        request = RequestResponseDefinition.new(&block)
+        request = RequestResponseDefinition.new(api, &block)
         api.request[:header] = request.header_param_ids
         api.request[:body] = request.body_param_ids
       end
@@ -29,16 +29,23 @@ module OneboxApiDoc
 
     def response &block
       if block_given?
-        response = RequestResponseDefinition.new(&block)
+        response = RequestResponseDefinition.new(api, &block)
         api.response[:header] = response.header_param_ids
         api.response[:body] = response.body_param_ids
       end
     end
 
-    class RequestResponseDefinition
-      attr_reader :header_param_ids, :body_param_ids
+    def error &block
+      if block_given?
+        errors = ErrorContainerDefinition.new(api, &block)
+      end
+    end
 
-      def initialize &block
+    class RequestResponseDefinition
+      attr_reader :api, :header_param_ids, :body_param_ids
+
+      def initialize api=nil, &block
+        @api = api
         @header_param_ids = []
         @body_param_ids = []
         self.instance_eval(&block) if block_given?
@@ -46,37 +53,65 @@ module OneboxApiDoc
 
       def header &block
         if block_given?
-          params = ParamContainerDefinition.new(&block)
+          params = ParamContainerDefinition.new(api, &block)
           @header_param_ids = params.param_ids
         end
       end
 
       def body &block
         if block_given?
-          params = ParamContainerDefinition.new(&block)
+          params = ParamContainerDefinition.new(api, &block)
           @body_param_ids = params.param_ids
         end
       end
     end
 
     class ParamContainerDefinition
-      attr_reader :parent_param, :param_ids
+      attr_reader :parent_object, :param_ids
 
-      def initialize parent_param=nil, &block
-        @parent_param = parent_param
+      def initialize parent_object=nil, &block
+        @parent_object = parent_object
         @param_ids = []
         self.instance_eval(&block) if block_given?
       end
 
       def param name, type, options={}, &block
-        options[:parent_id] = self.parent_param.object_id if self.parent_param.present?
-        param = OneboxApiDoc.base.add_param(name, type, options, &block)
+        options[:parent_id] = self.parent_object.object_id if self.parent_object.is_a? OneboxApiDoc::Param
+        if options[:permissions].present? and self.parent_object.doc.present?
+          options[:permission_ids] = options[:permissions].map { |permission_name| self.parent_object.doc.add_permission(permission_name).object_id }
+        end
+        param = self.parent_object.doc.add_param(name, type, options, &block)
         @param_ids << param.object_id
       end
     end
 
-    def error &block
-      # @_error = Error.new(&block) if block_given?
+    class ErrorContainerDefinition
+      attr_reader :api
+
+      def initialize api, &block
+        @api = api
+        self.instance_eval(&block) if block_given?
+      end
+
+      def code error_status, error_message="", &block
+        error = self.api.doc.add_error(api, error_status, error_message, &block)
+        api.error_ids << error.object_id
+      end
+    end
+
+    class ErrorDefinition < ParamContainerDefinition
+      attr_reader :permission_ids
+
+      def initialize parent_object, &block
+        @permission_ids = []
+        super
+      end
+
+      def permissions *permissions
+        @permission_ids = permissions.map{ |permission| parent_object.doc.add_permission(permission).object_id }
+      end
+
+
     end
 
   end
