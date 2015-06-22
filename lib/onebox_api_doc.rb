318dccffa1daf2014af0cb2c22c7a00052c54ce3
Carryall <carryall369@gmail.com>
2015-06-22 17:16:56 +0700

    remove unuse definition

--- lib/onebox_api_doc.rb
+++ lib/onebox_api_doc.rb
@@ -11,5 +11,4 @@ module OneboxApiDoc
   require "onebox_api_doc/onebox_api_doc_module"
   require "onebox_api_doc/route"
   require "onebox_api_doc/api_definition"
-  require "onebox_api_doc/param_definition"
 end
