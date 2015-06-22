f2b14ca45bfca21ab6a74ef53c3f699e62ff6462
Carryall <carryall369@gmail.com>
2015-06-22 16:55:57 +0700

    use base object class with new initialize method from active record

--- app/models/onebox_api_doc/error.rb
+++ app/models/onebox_api_doc/error.rb
@@ -1,13 +1,29 @@
 module OneboxApiDoc
-  class Error
+  class Error < BaseObject
     
-    attr_accessor :code, :message, :permission_ids, :param_ids
+    attr_accessor :doc_id, :code, :message, :permission_ids, :param_ids
 
-    def initialize code, message, permission_ids=[], param_ids=[]
-      self.code = code
-      self.message = message
-      self.permission_ids = permission_ids
-      self.param_ids = param_ids
+    def doc
+      OneboxApiDoc.base.docs.select { |doc| doc.object_id == self.doc_id }.first
+    end
+
+    def params
+      self.doc.params.select { |param| self.param_ids.include? param.object_id }
+    end
+
+    def permissions
+      self.doc.permissions.select { |permission| self.permission_ids.include? permission.object_id }
+    end
+
+    def permissions=(permission_names)
+      self.permission_ids = permission_names.map { |permission_name| self.doc.add_permission(name: permission_name) }
+    end
+
+    private
+
+    def set_default_value
+      self.permission_ids ||= []
+      self.param_ids ||= []
     end
   end
 end
--- test/dummy/spec/models/error_spec.rb
+++ test/dummy/spec/models/error_spec.rb
@@ -7,20 +7,37 @@ module OneboxApiDoc
     end
 
     describe "initialize" do
-      it "set correct code and message" do
-        error = OneboxApiDoc::Error.new 404, "Not Found"
+      it "set correct doc_id, code and message" do
+        error = OneboxApiDoc::Error.new doc_id: 1, code: 404, message: "Not Found"
         expect(error).not_to eq nil
+        expect(error.doc_id).to eq 1
         expect(error.code).to eq 404
         expect(error.message).to eq "Not Found"
       end
       it "set correct code, message, permission_ids and param_ids" do
-        error = OneboxApiDoc::Error.new 404, "Not Found", [1,3,5], [2,4,6]
+        error = OneboxApiDoc::Error.new doc_id: 1, code: 404, message: "Not Found", permission_ids: [1,3,5], param_ids: [2,4,6]
         expect(error).not_to eq nil
         expect(error.code).to eq 404
         expect(error.message).to eq "Not Found"
         expect(error.permission_ids).to eq [1,3,5]
         expect(error.param_ids).to eq [2,4,6]
       end
+    end
+
+    describe "doc" do
+      it "return correct doc"
+    end
+
+    describe "params" do
+      it "return correct params array"
+    end
+
+    describe "permissions" do
+      it "return correct permissions array"
+    end
+
+    describe "permissions=()" do
+      it "set correct permission_ids"
     end
   end
 end
