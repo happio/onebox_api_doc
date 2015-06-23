5319e0c143136de0976a4d372342f10bd36b2e15
Carryall <carryall369@gmail.com>
2015-06-23 16:25:45 +0700

    add detail on pending methods

--- app/models/onebox_api_doc/api.rb
+++ app/models/onebox_api_doc/api.rb
@@ -35,9 +35,15 @@ module OneboxApiDoc
     end
 
     def tags
+      doc.tags.select { |tag| self.tag_ids.include? tag.object_id }
+    end
+
+    def permissions
+      doc.permissions.select { |permission| self.permission_ids.include? permission.object_id }
     end
 
     def errors
+      doc.errors.select { |error| self.error_ids.include? error.object_id }
     end
 
     private
