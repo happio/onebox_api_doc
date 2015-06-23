686fc468647c575fb5f38602cbdc20d9c7237d3e
PiYa <piya23300@gmail.com>
2015-06-23 14:09:19 +0700

    fixed copy btn and redesign params containner

--- app/views/onebox_api_doc/application/show.js
+++ app/views/onebox_api_doc/application/show.js
@@ -1,1 +1,6 @@
-$("#api-details").html("<%=j render partial: 'onebox_api_doc/application/api_details', locals: { api: @api } %>");
+
+var details_container = $("#api-details");
+details_container.html("<%=j render partial: 'onebox_api_doc/application/api_details', locals: { api: @api } %>");
+
+var btn_copied = details_container.find('.copy--btn');
+bind_copy(btn_copied);
