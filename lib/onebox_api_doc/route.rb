298d468df8d77420bc1c3189e9ef60dbf3326c22
PiYa <piya23300@gmail.com>
2015-07-09 18:29:29 +0700

    fixed get routes for engine

--- lib/onebox_api_doc/route.rb
+++ lib/onebox_api_doc/route.rb
@@ -15,20 +15,80 @@ module OneboxApiDoc
         @all_api_routes ||= get_all_api_routes
       end
 
-      def get_all_api_routes
-        routes = Rails.application.routes.routes
-        routes.collect do |route|
-          ActionDispatch::Routing::RouteWrapper.new(route)
-        end.reject do |route|
-          route.internal?
-        end.collect do |route|
-          { name:   route.name,
-            method:   route.verb,
-            path:   route.path,
-            reqs:   route.reqs,
-            regexp: route.json_regexp }
+      # def get_all_api_routes
+      #   routes = Rails.application.routes.routes
+      #   routes.collect do |route|
+      #     ActionDispatch::Routing::RouteWrapper.new(route)
+      #   end.reject do |route|
+      #     route.internal?
+      #   end.collect do |route|
+      #     { name:   route.name,
+      #       method:   route.verb,
+      #       path:   route.path,
+      #       reqs:   route.reqs,
+      #       regexp: route.json_regexp }
+      #   end
+      # end
+      private
+        def get_all_api_routes
+          all_routes_from_app_and_engines = []
+
+          all_routes = Rails.application.routes.routes
+          require 'action_dispatch/routing/inspector'
+          inspector = ActionDispatch::Routing::RoutesInspector.new(all_routes)
+          routes_to_display = inspector.send(:filter_routes, ENV['CONTROLLER'])
+          all_routes = inspector.send(:collect_routes, routes_to_display)
+
+          # app routes
+          app_routes = get_routes_app(all_routes)
+          app_routes.each do |route|
+            all_routes_from_app_and_engines << route
+          end
+
+          # engines routes
+          engines = inspector.instance_variable_get :@engines
+          engines.each do |name, engine_routes|
+            prefix_path = get_prifix_engine_route(all_routes, name)
+            all_routes_from_app_and_engines += get_routes_engine(engine_routes, prefix_path)
+          end
+
+          # change formatt
+          all_routes_from_app_and_engines.collect do |route|
+            { 
+              name:   route[:name],
+              method: route[:verb],
+              path:   route[:path],
+              reqs:   route[:reqs]
+            }
+          end
         end
-      end
+
+        def get_prifix_engine_route all_routes, engine_name
+          all_routes.detect { |route| route[:reqs] == engine_name }[:path]
+        end
+
+        def get_routes_app all_routes
+          all_routes.select { |route| not route[:reqs].ends_with?("::Engine") }
+        end
+
+        def get_routes_engine engine_routes, prefix_path
+          routes = []
+          engine_routes.each do |route|
+            routes << route
+          end
+
+          routes.collect do |route|
+            { 
+              name:   route[:name],
+              method: route[:verb],
+              path:   "#{prefix_path}#{route[:path]}",
+              reqs:   route[:reqs]
+            }
+          end
+
+        end
+
+
     end
 
   end
--- test/dummy/spec/lib/route_spec.rb
+++ test/dummy/spec/lib/route_spec.rb
@@ -9,7 +9,6 @@ module OneboxApiDoc
           name: "user",
           method: "GET",
           path: "/users/:id",
-          regexp: "^\\/users\\/([^\\/.?]+)(?:\\.([^\\/.?]+))?$",
           reqs: "users#show"
         }
         expect(OneboxApiDoc::Route.route_for :users, :show).to eq expected_route
@@ -21,18 +20,13 @@ module OneboxApiDoc
 
     describe "get_all_api_routes" do
       it "return api routes with correct keys" do
-        routes = OneboxApiDoc::Route.get_all_api_routes
+        routes = OneboxApiDoc::Route.send(:get_all_api_routes)
         expect(routes.size).to be > 0
         routes.each do |route|
           expect(route.keys).to include(:name).
             and(include :method).and(include(:path)).
-            and(include(:reqs)).and(include(:regexp))
+            and(include(:reqs))
         end
-      end
-      it "return correct number of api routes (exclude internal routes)" do
-        routes = OneboxApiDoc::Route.get_all_api_routes
-        expected_routes = Rails.application.routes.routes.collect{ |route| ActionDispatch::Routing::RouteWrapper.new(route) }.reject{ |r| r.internal? }
-        expect(routes.size).to eq expected_routes.size
       end
     end
 
