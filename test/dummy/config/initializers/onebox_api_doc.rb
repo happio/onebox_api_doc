5da98d62dd0930409c75b04f085aac53d6314bcd
PiYa <piya23300@gmail.com>
2015-08-04 12:14:02 +0700

    .gitignore is now working

---
+++ test/dummy/config/initializers/onebox_api_doc.rb
@@ -0,0 +1,18 @@
+
+OneboxApiDoc::Engine.config do |config|
+
+  # app name
+  config.app_name = 'Api Doc'
+
+  # where is your API Doc defined?
+  config.root_resource = Rails.root
+  config.api_docs_matcher = "api_doc/**/*.rb"
+
+  # default version
+  config.default_version = "1.0.0"
+
+  # authentication
+  config.auth_method = "authenticate_developer!"
+
+
+end
