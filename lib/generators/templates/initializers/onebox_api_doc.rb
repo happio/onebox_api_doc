67c805c84ac66d38c86c6e3c9cadf6e007dae4c0
Carryall <carryall369@gmail.com>
2015-08-14 17:35:22 +0700

    change config syntag for path

--- lib/generators/templates/initializers/onebox_api_doc.rb
+++ lib/generators/templates/initializers/onebox_api_doc.rb
@@ -4,9 +4,13 @@ OneboxApiDoc::Engine.config do |config|
   # app name
   config.app_name = 'Api Doc'
 
-  # where is your API Doc defined?
-  config.root_resource = Rails.root
-  config.api_docs_matcher = "api_doc/**/*.rb"
+  # where is your API Doc and its load piorities
+  config.api_doc_paths do |doc|
+    # doc.path "path", root: your_root_path, priority: 0
+    # root is optional, default root is Rails.root
+    # priority is optional, default priority is 100
+    doc.path "api_doc/**/*.rb"
+  end
 
   # default version
   config.default_version = "0.0.0"
