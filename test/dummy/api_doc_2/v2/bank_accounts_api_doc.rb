5ed67f40f241c55e79724c23322117f11b3cc346
Carryall <carryall369@gmail.com>
2015-08-19 13:02:53 +0700

    fix test doc

--- test/dummy/api_doc_2/v2/bank_accounts_api_doc.rb
+++ test/dummy/api_doc_2/v2/bank_accounts_api_doc.rb
@@ -1,35 +1,37 @@
-module V2
-  class BankAccountsApiDoc < BaseV21ApiDoc
-    version "2.1.0"
-    resource_name :bank_accounts
+module ApiDoc2
+  module V2
+    class BankAccountsApiDoc < BaseV2ApiDoc
+      version "2.0.0"
+      resource_name :bank_accounts
 
-    get '/bank_accounts', 'get all bank_accounts' do
-      desc 'get all bank_accounts'
-      tags :web
-      permissions :guest
-      request do
-        header do
+      get '/bank_accounts', 'get all bank_accounts' do
+        desc 'get all bank_accounts'
+        tags :web
+        permissions :guest
+        request do
+          header do
+          end
+          body do
+          end
         end
-        body do
-        end
-      end
-      response do
-        body do
-          param "", :array,
-            desc: "array of bank account" do
-              param :id, :integer, 
-                desc: 'bank account id',
-                permissions: [ :guest ]
-              param :name, :string, 
-                desc: 'bank account name',
-                permissions: [ :guest ]
-              param :bank, :string, 
-                desc: 'bank account bank',
-                permissions: [ :guest ]
-              param :type, :string, 
-                desc: 'bank account type',
-                permissions: [ :guest ]
-            end
+        response do
+          body do
+            param "", :array,
+              desc: "array of bank account" do
+                param :id, :integer, 
+                  desc: 'bank account id',
+                  permissions: [ :guest ]
+                param :name, :string, 
+                  desc: 'bank account name',
+                  permissions: [ :guest ]
+                param :bank, :string, 
+                  desc: 'bank account bank',
+                  permissions: [ :guest ]
+                param :type, :string, 
+                  desc: 'bank account type',
+                  permissions: [ :guest ]
+              end
+          end
         end
       end
     end
