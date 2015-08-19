5f2a9a7ca1e50d1c28d7c4548eac719432953193
Carryall <carryall369@gmail.com>
2015-08-19 11:22:32 +0700

    add more doc for testing

---
+++ test/dummy/api_doc_2/base_v2_1_api_doc.rb
@@ -0,0 +1,11 @@
+class BaseV21ApiDoc < OneboxApiDoc::ApiDoc
+  version "2.1.0"
+
+  def_tags do
+    tag :web, 'Web', default: true
+  end
+
+  def_permissions do
+    permission :guest, 'Guest'
+  end
+end
---
+++ test/dummy/api_doc_2/v2/bank_accounts_api_doc.rb
@@ -0,0 +1,37 @@
+module V2
+  class BankAccountsApiDoc < BaseV21ApiDoc
+    version "2.1.0"
+    resource_name :bank_accounts
+
+    get '/bank_accounts', 'get all bank_accounts' do
+      desc 'get all bank_accounts'
+      tags :web
+      permissions :guest
+      request do
+        header do
+        end
+        body do
+        end
+      end
+      response do
+        body do
+          param "", :array,
+            desc: "array of bank account" do
+              param :id, :integer, 
+                desc: 'bank account id',
+                permissions: [ :guest ]
+              param :name, :string, 
+                desc: 'bank account name',
+                permissions: [ :guest ]
+              param :bank, :string, 
+                desc: 'bank account bank',
+                permissions: [ :guest ]
+              param :type, :string, 
+                desc: 'bank account type',
+                permissions: [ :guest ]
+            end
+        end
+      end
+    end
+  end
+end
